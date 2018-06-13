d511e8283992374ad7be9e3224c120e6f5469b13
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-06-13 02:25:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function currentLine(katzDeliLine) {
   } else {
     var lineArr = []
     for (var i = 0, l = katzDeliLine.length; i<l; i++) {
-      lineArr.push(`)
+      lineArr.push(`${i+1})
     }
     
     return "The line is currently: "
