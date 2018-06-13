7a34ef53dae02b6ae263d21aad6958d4ed522f45
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-06-13 02:13:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ function takeANumber(katzDeliLine, name) {
 }
 
 function nowServing(katzDeliLine) {
-  if (katzDeliLine.length>0) {
+  if (katzDeliLine>0) {
     return `Currently serving ${katzDeliLine.shift()}.`
   } else
   {return "There is nobody waiting to be served!"}
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty."
+  }
 }
