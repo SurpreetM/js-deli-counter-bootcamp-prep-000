15dbff7419a1ee6a3ea03c024eb4dffb9b12e561
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-06-13 02:23:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function currentLine(katzDeliLine) {
     return "The line is currently empty."
   } else {
     var lineArr = []
-    for (var i = 1; katzDeliLine.length)
+    for (var i = 0, l = katzDeliLine.length; i<l; )
     
     return "The line is currently: "
   }
